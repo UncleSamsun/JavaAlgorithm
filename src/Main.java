5e868f3c8cca46e64d367c02fdca9d5bcafad030
김민준 <alswns5620@naver.com>
2024-11-26 11:30:23 +0900

    Baekjoon 1157

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -5,17 +6,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String str = sc.nextLine();
+        String input = sc.nextLine();
+        String str = input.toUpperCase();
+        int max = 0;
+        int chNum = 0;
+        boolean flag = false;
 
-        StringBuffer sbStr = new StringBuffer(str);
+        int[] cnt = new int[26];
 
-        sbStr.reverse();
-
-        if(sbStr.toString().equals(str)) {
-            System.out.println("1");
+        for(int i = 0; i < str.length(); i++) {
+            cnt[str.charAt(i) - 'A']++;
         }
-        else {
-            System.out.println("0");
+
+        for(int i = 0; i < cnt.length; i++) {
+            if(cnt[i] > 0) {
+                if(max < cnt[i]) {
+                    max = cnt[i];
+                    chNum = i;
+                    flag = false;
+                } else if(max == cnt[i]) {
+                    flag = true;
+                }
+            }
+        }
+
+        if(flag) {
+            System.out.println("?");
+        } else {
+            System.out.println((char)('A'+chNum));
         }
     }
 }
