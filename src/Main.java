67baaec68a84c465997a988f11d445b427c1397c
김민준 <alswns5620@naver.com>
2024-11-29 10:38:22 +0900

    Baekjoon 2941

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,34 +5,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String input = sc.nextLine();
-        String str = input.toUpperCase();
-        int max = 0;
-        int chNum = 0;
-        boolean flag = false;
+        String str = sc.nextLine() + "11";
+        String[] strArr = str.split("");
+        int count = 0;
 
-        int[] cnt = new int[26];
-
-        for(int i = 0; i < str.length(); i++) {
-            cnt[str.charAt(i) - 'A']++;
-        }
-
-        for(int i = 0; i < cnt.length; i++) {
-            if(cnt[i] > 0) {
-                if(max < cnt[i]) {
-                    max = cnt[i];
-                    chNum = i;
-                    flag = false;
-                } else if(max == cnt[i]) {
-                    flag = true;
-                }
+        for (int i = 0; i < strArr.length-2; i++) {
+            if(strArr[i].equals("c") && strArr[i+1].equals("=")){
+                count++;
+                i++;
+            } else if (strArr[i].equals("c") && strArr[i+1].equals("-")) {
+                count++;
+                i++;
+            } else if (strArr[i].equals("d") && strArr[i+1].equals("-")) {
+                count++;
+                i++;
+            } else if (strArr[i].equals("l") && strArr[i+1].equals("j")) {
+                count++;
+                i++;
+            } else if (strArr[i].equals("n") && strArr[i+1].equals("j")) {
+                count++;
+                i++;
+            } else if (strArr[i].equals("s") && strArr[i+1].equals("=")) {
+                count++;
+                i++;
+            } else if (strArr[i].equals("z") && strArr[i+1].equals("=")) {
+                count++;
+                i++;
+            } else if (strArr[i].equals("d") && strArr[i+1].equals("z") && strArr[i+2].equals("=")) {
+                count++;
+                i += 2;
+            } else {
+                count++;
             }
         }
-
-        if(flag) {
-            System.out.println("?");
-        } else {
-            System.out.println((char)('A'+chNum));
-        }
+        System.out.println(count);
     }
 }
