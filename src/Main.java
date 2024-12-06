21f700a193eaf7964f42ad555ce2ea38e612b734
김민준 <alswns5620@naver.com>
2024-12-06 17:39:55 +0900

    Baekjoon 2738

--- src/Main.java
+++ src/Main.java
@@ -5,79 +5,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 평점 총 개수 = 20
-        int n = 20;
+        // 배열크기 입력
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
-        // 과목명, 학점, 평점 배열 선언
-        String[] className = new String[n];
-        String[] score = new String[n];
-        String[] grade = new String[n];
+        // 2차원 A배열, B배열 선언
+        int[][] arrA = new int[N][M];
+        int[][] arrB = new int[N][M];
 
-        // 전체 합, 학점 합, 학점 연산용 변수, 평점 연산용 변수
-        double sum = 0;
-        double scoreSum = 0;
-        double gradeNum = 0;
-        double scoreNum = 0;
-
-        // 학점 P 체크용
-        boolean flag = false;
-
-        // 과목별 학점, 평점 입력
-        for(int i = 0; i < n; i++) {
-            className[i] = sc.next();
-            score[i] = sc.next();
-            grade[i] = sc.next();
-        }
-
-        // 과목별 학점, 평점 합 구하기
-        for(int i = 0; i < n; i++) {
-            // 학점 double형으로 변환
-            switch (grade[i]) {
-                case "A+":
-                    gradeNum = 4.5;
-                    break;
-                case "A0":
-                    gradeNum = 4.0;
-                    break;
-                case "B+":
-                    gradeNum = 3.5;
-                    break;
-                case "B0":
-                    gradeNum = 3.0;
-                    break;
-                case "C+":
-                    gradeNum = 2.5;
-                    break;
-                case "C0":
-                    gradeNum = 2.0;
-                    break;
-                case "D+":
-                    gradeNum = 1.5;
-                    break;
-                case "D0":
-                    gradeNum = 1.0;
-                    break;
-                case "F":
-                    gradeNum = 0.0;
-                    break;
-                default:
-                    flag = true;
-                    break;
+        // A배열 입력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arrA[i][j] = sc.nextInt();
             }
-            // 학점이 P가 아닐시 학점, 평점 합 구하기
-            if (!flag) {
-                scoreNum = Double.parseDouble(score[i]);
-                scoreSum += scoreNum;
-                sum += scoreNum * gradeNum;
+        }
+
+        // B배열 입력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arrB[i][j] = sc.nextInt();
             }
-            // flag 초기화
-            flag = false;
         }
-        // 학점 합이 0이면 0.000000 출력, 아니면 소수점 6자로 출력
-        if (scoreSum == 0) {
-            System.out.println("0.000000");
-        } else {
-            System.out.println(String.format("%.6f", sum / scoreSum));
+
+        // A배열 + B배열 출력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(arrA[i][j] + arrB[i][j] + " ");
+            }
+            System.out.println();
         }
+
     }
 }
