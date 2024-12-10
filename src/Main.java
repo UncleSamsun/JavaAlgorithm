9a53a5f57a75fdc518fb9393b7b9ea4ad5eff102
김민준 <alswns5620@naver.com>
2024-12-10 16:24:06 +0900

    Baekjoon 2745

--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 색종이의 개수
-        int N = sc.nextInt();
-        // 색종이의 시작 좌표
-        int[][] paper = new int[N][2];
-        // 색종이 영역 : 100 x 100
-        boolean[][] area = new boolean[100][100];
-        // 색종이 영역 개수
-        int count = 0;
+        // String 입력
+        String str = sc.next();
+        // 진법 입력
+        int base = sc.nextInt();
+        // 합계
+        int sum = 0;
+        int tmp = 1;
 
-        // 색종이 시작 좌표 입력
-        for (int i = 0; i < N; i++) {
-            paper[i][0] = sc.nextInt();
-            paper[i][1] = sc.nextInt();
-        }
+        // 진법 변환
+        for (int i = str.length() - 1; i >= 0; i--) {
+            // 한 단어씩 계산
+            char c = str.charAt(i);
+            // 단어를 숫자로 변환
+            int value = 0;
 
-        // 색종이 영역 체크
-        for (int i = 0; i < N; i++) {
-            for (int j = paper[i][0]; j < paper[i][0] + 10; j++) {
-                for (int k = paper[i][1]; k < paper[i][1] + 10; k++) {
-                    area[j][k] = true;
-                }
+            if(c >= '0' && c <= '9') {
+                value = c - '0';
+            } else {
+                value = c - 'A' + 10;
             }
-        }
 
-        // 색종이 영역 개수 확인
-        for(int i = 0; i < 100; i++) {
-            for(int j = 0; j < 100; j++) {
-                if(area[i][j]) {
-                    count++;
-                }
-            }
+            // 진법 변환
+            sum += value * tmp;
+            tmp *= base;
         }
-        System.out.println(count);
-
+        System.out.println(sum);
     }
 }
