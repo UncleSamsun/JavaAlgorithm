8b823703d7f0f4f8bd288d12b4444d74e0699421
김민준 <alswns5620@naver.com>
2024-12-10 09:45:31 +0900

    Baekjoon 2563

--- src/Main.java
+++ src/Main.java
@@ -5,56 +5,39 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 입렵받을 문장 5줄 크기 선언
-        String[] str = new String[5];
-        // 각줄의 문장 길이
-        int[] len = new int[5];
-        // 각줄의 철자 카운트용 변수
-        int[] cnt = new int[5];
-        // 각 줄의 문장 끝 여부 확인용 플래그 변수
-        boolean[] flag = new boolean[5];
+        // 색종이의 개수
+        int N = sc.nextInt();
+        // 색종이의 시작 좌표
+        int[][] paper = new int[N][2];
+        // 색종이 영역 : 100 x 100
+        boolean[][] area = new boolean[100][100];
+        // 색종이 영역 개수
+        int count = 0;
 
-
-        // 각 줄의 문장 입력
-        for(int i = 0; i < 5; i++) {
-            str[i] = sc.next();
-            len[i] = str[i].length();
-            flag[i] = true;
+        // 색종이 시작 좌표 입력
+        for (int i = 0; i < N; i++) {
+            paper[i][0] = sc.nextInt();
+            paper[i][1] = sc.nextInt();
         }
 
-        // 각 줄의 문장이 모두 출력 될 때까지 반복
-        while(flag[0] || flag[1] || flag[2] || flag[3] || flag[4]) {
-            // 첫번째 줄부터 순서대로 출력
-            if (cnt[0] < len[0]) {
-                System.out.print(str[0].charAt(cnt[0]));
-                cnt[0]++;
-            } else {    // 문장이 끝났으면 false로 변경
-                flag[0] = false;
-            }
-            if (cnt[1] < len[1]) {
-                System.out.print(str[1].charAt(cnt[1]));
-                cnt[1]++;
-            } else {
-                flag[1] = false;
-            }
-            if (cnt[2] < len[2]) {
-                System.out.print(str[2].charAt(cnt[2]));
-                cnt[2]++;
-            } else {
-                flag[2] = false;
-            }
-            if (cnt[3] < len[3]) {
-                System.out.print(str[3].charAt(cnt[3]));
-                cnt[3]++;
-            } else {
-                flag[3] = false;
-            }
-            if (cnt[4] < len[4]) {
-                System.out.print(str[4].charAt(cnt[4]));
-                cnt[4]++;
-            } else {
-                flag[4] = false;
+        // 색종이 영역 체크
+        for (int i = 0; i < N; i++) {
+            for (int j = paper[i][0]; j < paper[i][0] + 10; j++) {
+                for (int k = paper[i][1]; k < paper[i][1] + 10; k++) {
+                    area[j][k] = true;
+                }
             }
         }
+
+        // 색종이 영역 개수 확인
+        for(int i = 0; i < 100; i++) {
+            for(int j = 0; j < 100; j++) {
+                if(area[i][j]) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+
     }
 }
