b2c48e2b77b9e990f79d1466fd1dacd0911c50f9
김민준 <alswns5620@naver.com>
2024-12-19 15:13:46 +0900

    Baekjoon 2501

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 반복
-        while (true) {
-            // 첫번째 수 입력
-            int num1 = sc.nextInt();
+        // 숫자 N 입력
+        int N = sc.nextInt();
+        // 숫자 K 입력
+        int K = sc.nextInt();
+        // count 변수
+        int count = 0;
+        int i = 0;
 
-            // 두번째 수 입력
-            int num2 = sc.nextInt();
+        // 약수 배열 선언
+        int[] divisor = new int[N];
 
-            // num1 과 num2가 둘다 0 일때 종료
-            if (num1 == 0 && num2 == 0) {
-                break;
-            }
-            // 첫번째 숫자가 두번째 숫자의 약수인지
-            if(num2 % num1 == 0) {
-                System.out.println("factor");
-            }
-            // 첫번째 숫자가 두번째 숫자의 배수인지
-            else if (num1 % num2 == 0) {
-                System.out.println("multiple");
-            }
-            // 둘다 아닌지
-            else {
-                System.out.println("neither");
+        while (count < N) {
+            count++;
+            // K의 배수인지 확인
+            if (N % count == 0) {
+                divisor[i] = count;
+                i++;
             }
         }
+        System.out.println(divisor[K - 1]);
     }
 }
