e1a1751625267396c8f7a57143f23efff59ba007
김민준 <alswns5620@naver.com>
2024-12-19 15:51:06 +0900

    Baekjoon 1978

--- src/Main.java
+++ src/Main.java
@@ -5,43 +5,34 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            // 수 입력
-            int N = sc.nextInt();
-            // 입력이 -1 이라면 중단
-            if(N == -1) {
-                break;
-            }
+        // N개의 수
+        int N = sc.nextInt();
+        // 개수 카운트
+        int count = 0;
 
-            // 카운트 변수
-            int cnt = 0;
-            // 약수의 합
-            int sum = 0;
-            // 약수 배열 선언
-            int[] div = new int[N];
+        // N 만큼 반복
+        for (int i = 0; i < N; i++) {
+            // flag
+            boolean flag = true;
+            // 입력
+            int num = sc.nextInt();
 
-            // N의 약수 찾기
-            for(int i = 1; i < N; i++) {
-                if(N % i == 0) {
-                    div[cnt] = i;
-                    sum += i;
-                    cnt++;
-                }
-            }
-
-            // N이 완전수인지 확인
-            if (sum == N) {
-                System.out.print(N + " = ");
-                for (int i = 0; i < cnt; i++) {
-                    System.out.print(div[i]);
-                    if (i != cnt - 1) {
-                        System.out.print(" + ");
+            // 입력된 수가 1이 아닐때
+            if (num != 1){
+                // 1과 자신을 제외한 수로 나머지를 구하기
+                for (int j = 2; j < num; j++) {
+                    // 1과 자신을 제외한 수 중에 약수가 있다면
+                    if (num % j == 0) {
+                        flag = false;
                     }
                 }
-                System.out.println();
-            } else {
-                System.out.println(N + " is NOT perfect.");
+                // 소수라면
+                if (flag) {
+                    count++;
+                }
             }
         }
+
+        System.out.println(count);
     }
 }
