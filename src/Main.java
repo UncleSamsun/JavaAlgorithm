892baac596e6ca37507a3695c0ed1ce70c16edc9
김민준 <alswns5620@naver.com>
2024-12-19 16:17:54 +0900

    Baekjoon 11653

--- src/Main.java
+++ src/Main.java
@@ -5,53 +5,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // M입력
-        int M = sc.nextInt();
-        // N입력
+        // 정수 N 입력
         int N = sc.nextInt();
-        // 소수의 합
-        int sum = 0;
-        // 소수 중 최솟값
-        int min = 0;
 
-        for (int i = M; i <= N; i++) {
-            // i가 1이면 continue
-            if (i == 1) {
-                continue;
+        // N이 소인수분해 될때까지 반복
+        for (int i = 2; i <= N; i++) {
+            while (N % i == 0) {
+                System.out.println(i);
+                N /= i;
             }
-
-            // flag
-            boolean flag = true;
-
-            // i의 소수
-            for (int j = 2; j < i; j++) {
-                // 소수가 아니라면
-                if (i % j == 0) {
-                    flag = false;
-                    break;
-                }
-            }
-
-            // 소수라면
-            if (flag) {
-                // 소수의 합
-                sum += i;
-
-                // 최솟값이 없다면 최솟값 입력
-                if (min == 0) {
-                    min = i;
-                }
-            }
-        }
-
-        // 소수가 있다면
-        if(sum != 0){
-            System.out.println(sum);
-            System.out.println(min);
-        }
-        // 소수가 없다면
-        else {
-            System.out.println(-1);
         }
     }
 }
