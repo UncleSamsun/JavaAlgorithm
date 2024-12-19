aa98ff9b757f919f2addca36323390a0172c05af
김민준 <alswns5620@naver.com>
2024-12-19 15:25:11 +0900

    Baekjoon 9506

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,43 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 숫자 N 입력
-        int N = sc.nextInt();
-        // 숫자 K 입력
-        int K = sc.nextInt();
-        // count 변수
-        int count = 0;
-        int i = 0;
+        while (true) {
+            // 수 입력
+            int N = sc.nextInt();
+            // 입력이 -1 이라면 중단
+            if(N == -1) {
+                break;
+            }
 
-        // 약수 배열 선언
-        int[] divisor = new int[N];
+            // 카운트 변수
+            int cnt = 0;
+            // 약수의 합
+            int sum = 0;
+            // 약수 배열 선언
+            int[] div = new int[N];
 
-        while (count < N) {
-            count++;
-            // K의 배수인지 확인
-            if (N % count == 0) {
-                divisor[i] = count;
-                i++;
+            // N의 약수 찾기
+            for(int i = 1; i < N; i++) {
+                if(N % i == 0) {
+                    div[cnt] = i;
+                    sum += i;
+                    cnt++;
+                }
+            }
+
+            // N이 완전수인지 확인
+            if (sum == N) {
+                System.out.print(N + " = ");
+                for (int i = 0; i < cnt; i++) {
+                    System.out.print(div[i]);
+                    if (i != cnt - 1) {
+                        System.out.print(" + ");
+                    }
+                }
+                System.out.println();
+            } else {
+                System.out.println(N + " is NOT perfect.");
             }
         }
-        System.out.println(divisor[K - 1]);
     }
 }
