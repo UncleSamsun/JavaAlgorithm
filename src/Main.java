2199ec8bbbdcd6efa18993cfde8beecd022c22a4
김민준 <alswns5620@naver.com>
2024-12-19 15:57:37 +0900

    Baekjoon 2581

--- src/Main.java
+++ src/Main.java
@@ -5,34 +5,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // N개의 수
+        // M입력
+        int M = sc.nextInt();
+        // N입력
         int N = sc.nextInt();
-        // 개수 카운트
-        int count = 0;
+        // 소수의 합
+        int sum = 0;
+        // 소수 중 최솟값
+        int min = 0;
 
-        // N 만큼 반복
-        for (int i = 0; i < N; i++) {
+        for (int i = M; i <= N; i++) {
+            // i가 1이면 continue
+            if (i == 1) {
+                continue;
+            }
+
             // flag
             boolean flag = true;
-            // 입력
-            int num = sc.nextInt();
 
-            // 입력된 수가 1이 아닐때
-            if (num != 1){
-                // 1과 자신을 제외한 수로 나머지를 구하기
-                for (int j = 2; j < num; j++) {
-                    // 1과 자신을 제외한 수 중에 약수가 있다면
-                    if (num % j == 0) {
-                        flag = false;
-                    }
+            // i의 소수
+            for (int j = 2; j < i; j++) {
+                // 소수가 아니라면
+                if (i % j == 0) {
+                    flag = false;
+                    break;
                 }
-                // 소수라면
-                if (flag) {
-                    count++;
+            }
+
+            // 소수라면
+            if (flag) {
+                // 소수의 합
+                sum += i;
+
+                // 최솟값이 없다면 최솟값 입력
+                if (min == 0) {
+                    min = i;
                 }
             }
         }
 
-        System.out.println(count);
+        // 소수가 있다면
+        if(sum != 0){
+            System.out.println(sum);
+            System.out.println(min);
+        }
+        // 소수가 없다면
+        else {
+            System.out.println(-1);
+        }
     }
 }
