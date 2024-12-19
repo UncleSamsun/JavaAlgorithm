00f314818f126ace0d5da245d4892a37af9240c6
김민준 <alswns5620@naver.com>
2024-12-19 17:12:32 +0900

    Baekjoon 9063

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,41 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 정사각형 n개 입력
-        // 값이 커서 long으로 선언
-        long n = sc.nextLong();
+        // 입력 개수 N
+        int N = sc.nextInt();
+        // 각 포인트 배열 선언
+        int[][] points = new int[2][N];
+        // x좌표, y좌표의 최솟값, 최댓값
+        int xMin = 100001;
+        int yMin = 100001;
+        int xMax = 0;
+        int yMax = 0;
 
-        // 직사각형의 둘레 출력
-        System.out.println(n * 4);
+        // N 만큼 반복
+        for (int i = 0; i < N; i++) {
+            points[0][i] = sc.nextInt();
+            points[1][i] = sc.nextInt();
+
+            // 각 x좌표, y좌표의 최대/최소 값 저장
+            if(xMin > points[0][i]) {
+                xMin = points[0][i];
+            }
+            if(xMax < points[0][i]) {
+                xMax = points[0][i];
+            }
+            if(yMin > points[1][i]) {
+                yMin = points[1][i];
+            }
+            if(yMax < points[1][i]) {
+                yMax = points[1][i];
+            }
+        }
+
+        // 가로 세로 길이 저장
+        int length = xMax - xMin;
+        int height = yMax - yMin;
+
+        // 면적 출력
+        System.out.println(length * height);
     }
 }
